425e89cebb1b1b74809d8afc3f1b7332fdbcd895
orkunincili <orkunincili0@gmail.com>
2021-04-05 00:10:30 +0300

    Added login API

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -52,6 +52,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
+
+
 ROOT_URLCONF = 'profiles_project.urls'
 
 TEMPLATES = [
